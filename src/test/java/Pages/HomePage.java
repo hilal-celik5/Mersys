deb132ffbebb99b83d1149cc74aa6df3f2f38dc5
Hilal Çelik <celikhilalx@gmail.com>
2025-06-15 11:53:55 +0300

    Add logo redirect to homepage

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,4 +1,16 @@
 package Pages;
 
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage extends ParentPage {
+
+    public HomePage() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+    @FindBy(xpath = "/html/body/app/student-layout/div/student-toolbar-horizontal/mat-toolbar/div/div[1]/div/div[1]/img")
+    public WebElement companyLogo;
 }
--- src/test/java/StepDefinitions/HomeNavigationSteps.java
+++ src/test/java/StepDefinitions/HomeNavigationSteps.java
@@ -1,5 +1,38 @@
 package StepDefinitions;
 
-public class HomeNavigationSteps {
-}
+import Pages.HomePage;
+import Utilities.GWD;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 
+import java.util.Set;
+
+public class HomeNavigationSteps {
+    HomePage homePage = new HomePage();
+
+    @When("The user clicks on the company logo")
+    public void the_user_clicks_on_the_company_logo() {
+        homePage.wait.until(ExpectedConditions.elementToBeClickable(homePage.companyLogo));
+        homePage.myClick(homePage.companyLogo);
+    }
+
+    @Then("The user should be redirected to the Techno Study homepage")
+    public void the_user_should_be_redirected_to_the_techno_study_homepage() {
+        String mainWindowHandle = GWD.getDriver().getWindowHandle();
+
+        homePage.wait.until(driver -> GWD.getDriver().getWindowHandles().size() > 1);
+        Set<String> windowHandles = GWD.getDriver().getWindowHandles();
+
+        for (String windowHandle : windowHandles) {
+            if (!windowHandle.equals(mainWindowHandle)) {
+                GWD.getDriver().switchTo().window(windowHandle);
+            }
+        }
+
+        homePage.wait.until(ExpectedConditions.urlContains("techno"));
+
+        Assert.assertEquals(GWD.getDriver().getCurrentUrl(), "https://techno.study/");
+    }
+}
