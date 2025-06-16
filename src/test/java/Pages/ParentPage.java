673fb0b20695fa1e9d1d1639c4be7d9f0758f320
Hilal Çelik <celikhilalx@gmail.com>
2025-06-16 20:44:02 +0300

    Add random class

--- src/test/java/Pages/ParentPage.java
+++ src/test/java/Pages/ParentPage.java
@@ -9,11 +9,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
+import java.util.Random;
 
 public class ParentPage {
     public WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
     public JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
     public Actions actions = new Actions(GWD.getDriver());
+    Random random = new Random();
 
     public void mySendKeys(WebElement e, String text) {
         wait.until(ExpectedConditions.visibilityOf(e));
