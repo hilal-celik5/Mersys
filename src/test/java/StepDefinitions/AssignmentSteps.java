0495fca25c5ab6764e33c21dc9a28aab31fa97c9
Görkem <gorkem.kezr@gmail.com>
2025-06-19 22:45:21 +0300

    steps bitti

--- src/test/java/StepDefinitions/AssignmentSteps.java
+++ src/test/java/StepDefinitions/AssignmentSteps.java
@@ -1,4 +1,35 @@
 package StepDefinitions;
 
+import Pages.AssignmentPage;
+import Utilities.GWD;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+
 public class AssignmentSteps {
+    AssignmentPage assignmentPage = new AssignmentPage();
+
+    @Given("Navigate to assignment page")
+    public void navigate_to_assignment_page() {
+        GWD.getDriver().get("https://test.mersys.io/assignments");
+    }
+
+    @When("User clicks submit button for assignment")
+    public void user_clicks_submit_button_for_assignment() {
+        assignmentPage.myClick(assignmentPage.submitButton);
+    }
+
+    @Then("User should see submission success message")
+    public void user_should_see_submission_success_message() {
+        assignmentPage.wait.until(ExpectedConditions.visibilityOf(assignmentPage.successMessage));
+        Assert.assertTrue(assignmentPage.successMessage.isDisplayed());
+    }
+
+    @And("User clicks start discussion for assignment")
+    public void user_clicks_start_discussion_for_assignment() {
+        assignmentPage.myClick(assignmentPage.startDiscussionButton);
+    }
 }
--- src/test/java/StepDefinitions/CalendarSteps.java
+++ src/test/java/StepDefinitions/CalendarSteps.java
@@ -1,4 +1,36 @@
 package StepDefinitions;
 
+import Pages.CalendarPage;
+import Utilities.GWD;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+
 public class CalendarSteps {
+    CalendarPage calendarPage = new CalendarPage();
+
+    @Given("Navigate to calendar page")
+    public void navigate_to_calendar_page() {
+        GWD.getDriver().get("https://test.mersys.io/calendar");
+    }
+
+    @When("User switches to weekly view")
+    public void user_switches_to_weekly_view() {
+        calendarPage.myClick(calendarPage.weeklyViewButton);
+    }
+
+    @Then("Calendar should display weekly schedule")
+    public void calendar_should_display_weekly_schedule() {
+        calendarPage.wait.until(ExpectedConditions.visibilityOf(calendarPage.calendarDay));
+        Assert.assertTrue(calendarPage.calendarDay.isDisplayed());
+    }
+
+    @And("User sees ended recording label")
+    public void user_sees_ended_recording_label() {
+        calendarPage.wait.until(ExpectedConditions.visibilityOf(calendarPage.endedRecordingMessage));
+        Assert.assertTrue(calendarPage.endedRecordingMessage.isDisplayed());
+    }
 }
