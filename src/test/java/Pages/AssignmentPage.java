75ad267b52ecd3071f8061958071981af46b8bab
Görkem <gorkem.kezr@gmail.com>
2025-06-19 22:44:18 +0300

    pages bitti

--- src/test/java/Pages/AssignmentPage.java
+++ src/test/java/Pages/AssignmentPage.java
@@ -1,4 +1,31 @@
 package Pages;
 
-public class AssignmentPage {
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AssignmentPage extends ParentPage {
+
+    public AssignmentPage() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+    @FindBy(css = "[data-testid='assignments-tab']")
+    public WebElement assignmentsTab;
+
+    @FindBy(css = ".assignment-item")
+    public WebElement assignmentItem;
+
+    @FindBy(css = ".assignment-title")
+    public WebElement assignmentTitle;
+
+    @FindBy(css = ".submit-button")
+    public WebElement submitButton;
+
+    @FindBy(css = ".submission-success-message")
+    public WebElement successMessage;
+
+    @FindBy(css = ".discussion-start-button")
+    public WebElement startDiscussionButton;
 }
--- src/test/java/Pages/CalendarPage.java
+++ src/test/java/Pages/CalendarPage.java
@@ -1,4 +1,28 @@
 package Pages;
 
-public class CalendarPage {
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CalendarPage extends ParentPage {
+
+    public CalendarPage() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+    @FindBy(css = "[data-testid='calendar-tab']")
+    public WebElement calendarTab;
+
+    @FindBy(css = ".calendar-view-toggle button[data-view='weekly']")
+    public WebElement weeklyViewButton;
+
+    @FindBy(css = ".calendar-day")
+    public WebElement calendarDay;
+
+    @FindBy(css = ".calendar-event")
+    public WebElement calendarEvent;
+
+    @FindBy(css = ".recording-ended-message")
+    public WebElement endedRecordingMessage;
 }
