bc901f7d1233d61fc713cd4e955e73edbd84e4de
Hilal Çelik <celikhilalx@gmail.com>
2025-06-22 17:46:40 +0300

    Add TestRunnerAll

---
+++ src/test/java/Runners/TestRunnerAll.java
@@ -0,0 +1,13 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/java/FeatureFiles",
+        glue = "StepDefinitions",
+        plugin = "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+        dryRun = false
+)
+public class TestRunnerAll extends AbstractTestNGCucumberTests {
+}
